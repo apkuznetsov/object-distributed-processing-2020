43eb3fe3c8a03c4974b7c5b79b3e5ef672770107
Andrey <kouznts@ya.ru>
2020-02-22 18:43:59 +0400

    Dao interface

--- Lab4/src/main/java/com/haulmont/testtask/Dao/Dao.java
+++ Lab4/src/main/java/com/haulmont/testtask/Dao/Dao.java
@@ -1,70 +1,16 @@
 package com.haulmont.testtask.Dao;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public abstract class Dao {
-    protected String jdbcDriver = null;
-    protected String dbUrl = null;
+public interface Dao {
+    void connect() throws ClassNotFoundException, SQLException;
 
-    protected String user = null;
-    protected String password = null;
+    void disconnect() throws SQLException;
 
-    private Connection connection = null;
+    boolean execute(final String sqlQuery) throws SQLException;
 
-    protected Dao(String jdbcDriver, String dbUrl, String user, String password) {
-        this.jdbcDriver = jdbcDriver;
-        this.dbUrl = dbUrl;
-        this.user = user;
-        this.password = password;
-    }
+    ResultSet executeQuery(final String sqlQuery) throws SQLException;
 
-    public void connect() throws ClassNotFoundException, SQLException {
-        Class.forName(jdbcDriver);
-        connection = DriverManager.getConnection(dbUrl, user, password);
-    }
-
-    public void disconnect() throws SQLException {
-        if (connection != null) {
-            connection.close();
-        }
-    }
-
-    public boolean execute(final String sqlQuery) throws SQLException {
-        boolean result = false;
-
-        if (connection != null) {
-            Statement statement = connection.createStatement();
-            result = statement.execute(sqlQuery);
-
-            statement.close();
-        }
-
-        return result;
-    }
-
-    public ResultSet executeQuery(final String sqlQuery) throws SQLException {
-        ResultSet resultSet = null;
-
-        if (connection != null) {
-            Statement statement = connection.createStatement();
-            resultSet = statement.executeQuery(sqlQuery);
-
-            statement.close();
-        }
-
-        return resultSet;
-    }
-
-    public int executeUpdate(final String sqlQuery) throws SQLException {
-        int changedRowsNum = -1;
-
-        if (connection != null) {
-            Statement statement = connection.createStatement();
-            changedRowsNum = statement.executeUpdate(sqlQuery);
-
-            statement.close();
-        }
-
-        return changedRowsNum;
-    }
+    int executeUpdate(final String sqlQuery) throws SQLException;
 }
--- Lab4/src/main/java/com/haulmont/testtask/Dao/HsqldbDao.java
+++ Lab4/src/main/java/com/haulmont/testtask/Dao/HsqldbDao.java
@@ -1,6 +1,6 @@
 package com.haulmont.testtask.Dao;
 
-public class HsqldbDao extends Dao {
+public class HsqldbDao extends AbstractDao {
     private static final String JDBC_DRIVER = "org.hsqldb.jdbc.JDBCDriver";
 
     public HsqldbDao(String dbUrl, String user, String password) {
