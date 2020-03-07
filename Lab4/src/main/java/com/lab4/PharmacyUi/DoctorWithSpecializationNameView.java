e23ad2945607446cd8d3fcc51e422a31894813e6
Andrey <kouznts@ya.ru>
2020-03-07 12:13:35 +0400

    DoctorWithSpecializationNameView

---
+++ Lab4/src/main/java/com/lab4/PharmacyUi/DoctorWithSpecializationNameView.java
@@ -0,0 +1,71 @@
+package com.lab4.PharmacyUi;
+
+import com.lab4.PharmacyDb.Daos.DoctorDao;
+import com.lab4.PharmacyDb.Dtos.Doctor;
+import com.lab4.PharmacyDb.Dtos.DoctorSpecialization;
+import com.lab4.PharmacyDb.Dtos.DoctorWithSpecializationName;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static com.lab4.MainUI.pharmacyDbDao;
+
+public class DoctorWithSpecializationNameView extends VerticalLayout implements View {
+    private DoctorDao doctorDao;
+
+    private Grid<Doctor> grid;
+    private HorizontalLayout gridLayout;
+
+    public DoctorWithSpecializationNameView() {
+        // region поля
+        doctorDao = pharmacyDbDao.getDoctorDao();
+
+        grid = new Grid<>(DoctorWithSpecializationName.class);
+        gridLayout = new HorizontalLayout(grid);
+        // endregion
+
+        setDoctorsGrid();
+        gridLayout.setSizeFull();
+        gridLayout.setExpandRatio(grid, 1);
+
+        addComponents(gridLayout);
+        updateDoctorsGrid();
+    }
+
+    private void setDoctorsGrid() {
+        grid.setColumns(
+                Doctor.ID,
+                Doctor.SURNAME,
+                Doctor.FORENAME,
+                Doctor.PATRONYMIC,
+                DoctorSpecialization.NAME);
+
+        grid.getColumn(Doctor.ID).setCaption("Номер");
+        grid.getColumn(Doctor.SURNAME).setCaption("Фамилия");
+        grid.getColumn(Doctor.FORENAME).setCaption("Имя");
+        grid.getColumn(Doctor.PATRONYMIC).setCaption("Отчество");
+        grid.getColumn(DoctorSpecialization.NAME).setCaption("Специализация");
+
+        grid.setSizeFull();
+    }
+
+    public void updateDoctorsGrid() {
+        try {
+            List<Doctor> doctors = doctorDao.getAllDoctorsWithSpecializationName();
+            grid.setItems(doctors);
+        } catch (SQLException | ClassNotFoundException exc) {
+            Notification.show(exc.getMessage());
+            exc.printStackTrace();
+        }
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+    }
+}
