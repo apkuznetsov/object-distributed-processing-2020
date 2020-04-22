1cbf35a7c642c2e62098c3c471a7532d9152050a
Andrey <kouznts@ya.ru>
2020-04-22 10:36:29 +0400

    printDoctorsWithSpecializationName()

--- lab/src/main/java/Main.java
+++ lab/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import DoctorDb.Daos.DoctorDao;
 import DoctorDb.DoctorDbDao;
 import DoctorDb.Dtos.Doctor;
+import DoctorDb.Dtos.DoctorWithSpecializationName;
 import DoctorDb.HsqldbDaos.HsqldbDoctorDbDao;
 
 import java.sql.SQLException;
@@ -14,7 +15,8 @@ public class Main {
     private static final DoctorDao doctorDao = dbDao.getDoctorDao();
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        printDoctors();
+        System.out.println("1 -- список врачей");
+        printDoctorsWithSpecializationName();
     }
 
     private static void printDoctors() throws SQLException, ClassNotFoundException {
@@ -28,6 +30,20 @@ public class Main {
                     d.getForename(),
                     d.getPatronymic(),
                     d.getSpecializationId());
+        }
+    }
+
+    private static void printDoctorsWithSpecializationName() throws SQLException, ClassNotFoundException {
+        List<DoctorWithSpecializationName> doctors = doctorDao.getAllDoctorsWithSpecializationName();
+
+        System.out.printf("%-5s %-15s %-15s %-15s %-20s\n", "№", "Фамилия", "Имя", "Отчество", "Специализации");
+        for (DoctorWithSpecializationName d : doctors) {
+            System.out.printf("%-5s %-15s %-15s %-15s %-20s\n",
+                    d.getId(),
+                    d.getSurname(),
+                    d.getForename(),
+                    d.getPatronymic(),
+                    d.getSpecializationName());
         }
     }
 }
